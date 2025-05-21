f0affcc63996678aaf4e55189304b1d9c7a99c7c
Fernando Garcia <fg1998@gmail.com>
2025-05-21 17:18:08 -0300

    fix performa and amiga

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function createWindow() {
   });
 
   win.loadFile('renderer/index.html');
-  win.webContents.openDevTools(); // DevTools ativado
+  //win.webContents.openDevTools(); // DevTools ativado
 }
 
 app.whenReady().then(() => {
