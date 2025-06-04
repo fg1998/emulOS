5e241ce5d8778d8aea5475c2e9d765ae296a95e6
Fernando Garcia <fg1998@gmail.com>
2025-06-04 20:14:32 -0300

    install.sh (missing NVM commands)

--- docs/install.sh
+++ docs/install.sh
@@ -2,6 +2,12 @@
 set -x
 sudo apt update -y
 
+#git
+#sudo apt install git -y
+#git clone https://github.com/fg1998/emulOS.git
+#sudo apt-get install curl -y
+
+
 #install X
 sudo apt install --no-install-recommends \
   xserver-xorg \
@@ -12,15 +18,14 @@ sudo apt install --no-install-recommends \
 #xterm
 sudo apt install xterm -y
 
-#git
-sudo apt-get install git -y
-git clone https://github.com/fg1998/emulOS.git
 sudo apt-get install curl -y
 
 #NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-** MISSING EXPORT NVM COMMANDS
-
+##MISSING EXPORT NVM COMMANDS
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 #NODE
 nvm install --lts
 
