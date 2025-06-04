ee26de4ea8da92a1d322e8270acb499b930b84e6
Fernando Garcia <fg1998@gmail.com>
2025-06-04 19:58:50 -0300

    Install script

--- docs/install.sh
+++ docs/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo apt update
+set -x
+sudo apt update -y
 
 #install X
 sudo apt install --no-install-recommends \
@@ -8,9 +9,10 @@ sudo apt install --no-install-recommends \
   openbox \
   matchbox-window-manager -y
 
+#xterm
 sudo apt install xterm -y
 
-
+#git
 sudo apt-get install git -y
 git clone https://github.com/fg1998/emulOS.git
 sudo apt-get install curl -y
@@ -19,54 +21,46 @@ sudo apt-get install curl -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 ** MISSING EXPORT NVM COMMANDS
 
+#NODE
 nvm install --lts
 
 
+## FUSE
+#sudo apt install libsdl1.2-dev libpng-dev zlib1g-dev libbz2-dev libaudiofile-dev bison flex devscripts x11proto-core-dev libdirectfb-dev libraspberrypi-dev -y
 
+#gdown
+python3 -m pip install --break-system-packages --user gdown
+
+#Amiberry
+sudo apt install build-essential git cmake libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libflac-dev libmpg123-dev libpng-dev libmpeg2-4-dev libserialport-dev libportmidi-dev libenet-dev -y
+
+#coco
+sudo apt install libsdl2-dev automake libasound2-dev libsndfile1-dev texinfo zlib1g-dev libpulse-dev -y
+
+#dosbox
+sudo apt install libsdl-sound1.2 libsdl-net1.2
+
+#linapple
+sudo apt install libzip-dev libsdl1.2-dev libsdl-image1.2-dev libcurl4-openssl-dev libzip-dev libsdl1.2-dev libsdl-image1.2-dev libcurl4-openssl-dev zlib1g-dev imagemagick -y
+
+#miniVmac
+sudo apt install libsdl2-dev -y
+
+#openMSX
+sudo apt install libsdl2-dev libsdl2-ttf-dev libao-dev libogg-dev libtheora-dev libxml2-dev libvorbis-dev tcl-dev libasound2-dev libfreetype6-dev libglew-dev -y
+
+#sdlTRS
+sudo apt install libsdl2-dev libreadline-dev -y
+
+#vice
+sudo apt install libsdl2-dev libsdl2-image-dev libmpg123-dev libpng-dev zlib1g-dev libasound2-dev libvorbis-dev libflac-dev libpcap-dev automake bison flex libjpeg-dev portaudio19-dev xa65 dos2unix -y
+
+#ZEsarUX
+sudo apt-get install libssl-dev libpthread-stubs0-dev libasound2-dev libsdl2-dev -y
 
 cd emulOS
 
-
-
-echo "Install fuse"
-wget https://files.retropie.org.uk/archives/fuse-1.5.7.tar.gz
-mkdir fuse
-tar -xvf ./fuse-1.5.7.tar.gz -C ./fuse --strip-components 1
-rm fuse-1.5.7.tar.gz
-
-wget https://files.retropie.org.uk/archives/libspectrum-1.4.4.tar.gz
-mkdir fuse/libspectrum
-tar -xvf ./libspectrum-1.4.4.tar.gz -C ./fuse/libspectrum --strip-components 1
-rm ./libspectrum-1.4.4.tar.gz
-
-cd fuse/libspectrum
-./configure --disable-shared
-make clean
-make
-
-cd ..
-mkdir bin
-./configure --prefix="/home/fg1998/Downloads/fuse/bin" --without-libao --without-gpm --without-gtk --without-libxml2 --with-sdl LIBSPECTRUM_CFLAGS="-Ilibspectrum" LIBSPECTRUM_LIBS="-Llibspectrum/.libs -lspectrum"
-make clean
-make
-
-//dependencias
-libsdl1.2-dev libpng-dev zlib1g-dev libbz2-dev libaudiofile-dev bison flex
-devscripts x11proto-core-dev libdirectfb-dev libraspberrypi-dev
-
-descompactar o fuse em uma pasta
-descompactar o libspetrum em uma pasta DENTRO da pasta do fuse
-
-dentro da pasta libspectrum
- ./configure --disable-shared
-    make clean
-    make
-
-
-dentro da pasta do fuse
-./configure --prefix="fuse" --without-libao --without-gpm --without-gtk --without-libxml2 --with-sdl LIBSPECTRUM_CFLAGS="-Ilibspectrum-1.4.4" LIBSPECTRUM_LIBS="-Llibspectrum-1.4.4/.libs -lspectrum"
-
-make
+npm install && npm start
 
 
 
