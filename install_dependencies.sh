98115a31767ddb735b31ccf523696bf7f3750e49
Fernando Garcia <fg1998@gmail.com>
2025-06-25 20:30:08 -0300

    Fixing dependencies on 64bits SO

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -14,7 +14,7 @@ error_handler() {
 trap error_handler ERR
 
 # Atualiza o sistema
-sudo apt-get update -y
+#sudo apt-get update -y
 
 # Instala o mínimo necessário do X e openbox
 sudo apt-get install --no-install-recommends \
@@ -30,43 +30,6 @@ sudo apt-get install --no-install-recommends \
   gpm \
   dialog \
   curl \
-  -y
-
-# Configura o xinitrc
-cat > ~/.xinitrc << 'EOF'
-#!/bin/sh
-
-# Ajuste de DPI para o Pi400, melhora o xterm e fontes
-xrdb -merge <<EOD
-Xft.dpi: 96
-XTerm*faceName: DejaVu Sans Mono
-XTerm*faceSize: 12
-EOD
-
-xterm &
-exec openbox
-EOF
-
-chmod +x ~/.xinitrc
-
-# Python e gdown para downloads
-sudo apt-get install python3 python3-pip -y
-python3 -m pip install --break-system-packages --user gdown
-
-# Ajusta o PATH caso ~/.local/bin não esteja
-if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
-  echo 'export PATH=$PATH:$HOME/.local/bin' >> ~/.bashrc
-  export PATH=$PATH:$HOME/.local/bin
-fi
-
-# Faz o download dos binários compactados (substitua pelo ID correto)
-gdown -id 1Ooj-wX8HZBU3yDXbs338Wya9_fVqO_2w
-
-# Descompacta para o sistema
-sudo tar -xvzf /home/emulos/emulators.tar.gz -C / --strip-components=1
-
-# Instala bibliotecas runtime necessárias para os binários rodarem (Electron + emuladores)
-sudo apt-get install \
   libatk-bridge2.0-0 \
   libgtk-3-0 \
   libnss3 \
@@ -82,8 +45,26 @@ sudo apt-get install \
   libxkbcommon0 \
   libxkbcommon-x11-0 \
   alsa-utils alsa-oss \
-  systemd systemd-sysv pulseaudio \
+  systemd \
+  systemd-sysv \
+  pulseaudio \
+  libsdl2-2.0-0 \
+  libserialport \
+  libportmidi0 \
+  libmpeg2-4 \
+  libnet1 \
   -y
+
+
+
+
+
+# Ajusta o PATH caso ~/.local/bin não esteja
+if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
+  echo 'export PATH=$PATH:$HOME/.local/bin' >> ~/.bashrc
+  export PATH=$PATH:$HOME/.local/bin
+fi
+
 
 # Inicia o pulseaudio (precisa para alguns emuladores)
 systemctl --user start pulseaudio
